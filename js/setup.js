588c60705ff3f1841fa88c317f2f29ae5f817c64
Spektorn <filonenko.zlata@mail.ru>
2020-06-14 01:39:15 +0300

    Открытие и закрытие setup'а.

--- js/setup.js
+++ js/setup.js
@@ -37,11 +37,12 @@ var EYES_COLOR_PATTERN = [
 ];
 var WIZARDS_QUANTITY = 4;
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var getRandomArrValue = function (patternName) {
+  return patternName[getRandomInt(0, patternName.length - 1)];
 };
 
 var getRandomWizards = function () {
@@ -49,9 +50,9 @@ var getRandomWizards = function () {
 
   for (var i = 0; i < WIZARDS_QUANTITY; i++) {
     var newWizard = {
-      name: NAME_PATTERN[getRandomInt(0, NAME_PATTERN.length - 1)] + ' ' + SURNAME_PATTERN[getRandomInt(0, SURNAME_PATTERN.length - 1)],
-      coatColor: COAT_COLOR_PATTERN[getRandomInt(0, COAT_COLOR_PATTERN.length - 1)],
-      eyesColor: EYES_COLOR_PATTERN[getRandomInt(0, EYES_COLOR_PATTERN.length - 1)],
+      name: getRandomArrValue(NAME_PATTERN) + ' ' + getRandomArrValue(SURNAME_PATTERN),
+      coatColor: getRandomArrValue(COAT_COLOR_PATTERN),
+      eyesColor: getRandomArrValue(EYES_COLOR_PATTERN),
     };
 
     wizards.push(newWizard);
@@ -60,22 +61,17 @@ var getRandomWizards = function () {
   return wizards;
 };
 
-var wizards = getRandomWizards();
-
-var similarWizardList = setup.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
 var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var newWizard = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  newWizard.querySelector('.setup-similar-label').textContent = wizard.name;
+  newWizard.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  newWizard.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-  return wizardElement;
+  return newWizard;
 };
 
-var createWizardsFragment = function () {
+var renderWizardsFragment = function (wizards) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < wizards.length; i++) {
@@ -85,10 +81,57 @@ var createWizardsFragment = function () {
   return fragment;
 };
 
-var fragment = createWizardsFragment();
+var wizards = getRandomWizards();
 
-similarWizardList.appendChild(fragment);
+var setupElement = document.querySelector('.setup');
 
-setup.querySelector('.setup-similar').classList.remove('hidden');
+var similarWizardList = setupElement.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
+var wizardsFragment = renderWizardsFragment(wizards);
+similarWizardList.appendChild(wizardsFragment);
 
+setupElement.querySelector('.setup-similar').classList.remove('hidden');
+
+var setupOpenButtonElement = document.querySelector('.setup-open');
+var setupOpenIconElement = setupOpenButtonElement.querySelector('.setup-open-icon');
+var setupCloseButtonElement = setupElement.querySelector('.setup-close');
+
+var setupCloseClickHandler = function (evt) {
+  evt.preventDefault();
+  setupElement.classList.add('hidden');
+
+  setupCloseButtonElement.removeEventListener('click', setupCloseClickHandler);
+  document.removeEventListener('keydown', setupClosePressEscHandler);
+};
+
+var setupClosePressEscHandler = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    setupElement.classList.add('hidden');
+  }
+
+  setupCloseButtonElement.removeEventListener('click', setupCloseClickHandler);
+  document.removeEventListener('keydown', setupClosePressEscHandler);
+};
+
+var setupOpenClickHandler = function (evt) {
+  evt.preventDefault();
+  setupElement.classList.remove('hidden');
+
+  setupCloseButtonElement.addEventListener('click', setupCloseClickHandler);
+  document.addEventListener('keydown', setupClosePressEscHandler);
+};
+
+var setupOpenPressEnterHandler = function (evt) {
+  if (evt.key === 'Enter') {
+    evt.preventDefault();
+    setupElement.classList.remove('hidden');
+  }
+
+  setupCloseButtonElement.addEventListener('click', setupCloseClickHandler);
+  document.addEventListener('keydown', setupClosePressEscHandler);
+};
+
+setupOpenButtonElement.addEventListener('click', setupOpenClickHandler);
+setupOpenIconElement.addEventListener('keydown', setupOpenPressEnterHandler);
