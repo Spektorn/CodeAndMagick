02d76709aa98c74bd98bfa54d2c4e34f92cf550a
Spektorn <filonenko.zlata@mail.ru>
2020-06-14 03:47:11 +0300

    Доработка открытия и закрытия setup'а. Изменение цветов мага. Отправка формы с валидацией.

--- js/setup.js
+++ js/setup.js
@@ -35,6 +35,14 @@ var EYES_COLOR_PATTERN = [
   'yellow',
   'green',
 ];
+var FIREBALL_COLOR_PATTERN = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848',
+];
+
 var WIZARDS_QUANTITY = 4;
 
 var getRandomInt = function (min, max) {
@@ -81,9 +89,79 @@ var renderWizardsFragment = function (wizards) {
   return fragment;
 };
 
+var openSetup = function (evt) {
+  evt.preventDefault();
+  setupElement.classList.remove('hidden');
+
+  setupCloseButtonElement.addEventListener('click', setupCloseClickHandler);
+  setupCloseButtonElement.addEventListener('keydown', setupClosePressEnterHandler);
+  document.addEventListener('keydown', setupClosePressEscHandler);
+  setupNameInputElement.addEventListener('focus', usernameInputFocusHandler);
+  setupNameInputElement.addEventListener('blur', usernameInputBlurHandler);
+};
+
+var closeSetup = function (evt) {
+  evt.preventDefault();
+  setupElement.classList.add('hidden');
+
+  setupCloseButtonElement.removeEventListener('click', setupCloseClickHandler);
+  setupCloseButtonElement.removeEventListener('keydown', setupClosePressEnterHandler);
+  document.removeEventListener('keydown', setupClosePressEscHandler);
+  setupNameInputElement.removeEventListener('focus', usernameInputFocusHandler);
+  setupNameInputElement.removeEventListener('blur', usernameInputBlurHandler);
+};
+
+var usernameInputFocusHandler = function () {
+  document.removeEventListener('keydown', setupClosePressEscHandler);
+};
+
+var usernameInputBlurHandler = function () {
+  document.addEventListener('keydown', setupClosePressEscHandler);
+};
+
+var setupCloseClickHandler = function (evt) {
+  closeSetup(evt);
+};
+
+var setupClosePressEnterHandler = function (evt) {
+  if (evt.key === 'Enter') {
+    closeSetup(evt);
+  }
+};
+
+var setupClosePressEscHandler = function (evt) {
+  if (evt.key === 'Escape') {
+    closeSetup(evt);
+  }
+};
+
+var setupOpenClickHandler = function (evt) {
+  openSetup(evt);
+};
+
+var setupOpenPressEnterHandler = function (evt) {
+  if (evt.key === 'Enter') {
+    openSetup(evt);
+  }
+};
+
 var wizards = getRandomWizards();
 
 var setupElement = document.querySelector('.setup');
+
+var setupOpenButtonElement = document.querySelector('.setup-open');
+var setupOpenIconElement = setupOpenButtonElement.querySelector('.setup-open-icon');
+var setupCloseButtonElement = setupElement.querySelector('.setup-close');
+
+var setupWizardElement = setupElement.querySelector('.setup-wizard');
+var setupWizardFireballElement = setupElement.querySelector('.setup-fireball-wrap');
+var setupWizardCoatElement = setupWizardElement.querySelector('.wizard-coat');
+var setupWizardEyesElement = setupWizardElement.querySelector('.wizard-eyes');
+
+var setupNameInputElement = setupElement.querySelector('#username-modal');
+var setupFireballInputElement = setupWizardFireballElement.querySelector('#fireball-color-modal');
+var setupCoatInputElement = setupElement.querySelector('#coat-color-modal');
+var setupEyesInputElement = setupElement.querySelector('#eyes-color-modal');
 
 var similarWizardList = setupElement.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -93,45 +171,26 @@ similarWizardList.appendChild(wizardsFragment);
 
 setupElement.querySelector('.setup-similar').classList.remove('hidden');
 
-var setupOpenButtonElement = document.querySelector('.setup-open');
-var setupOpenIconElement = setupOpenButtonElement.querySelector('.setup-open-icon');
-var setupCloseButtonElement = setupElement.querySelector('.setup-close');
-
-var setupCloseClickHandler = function (evt) {
-  evt.preventDefault();
-  setupElement.classList.add('hidden');
-
-  setupCloseButtonElement.removeEventListener('click', setupCloseClickHandler);
-  document.removeEventListener('keydown', setupClosePressEscHandler);
-};
-
-var setupClosePressEscHandler = function (evt) {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    setupElement.classList.add('hidden');
-  }
-
-  setupCloseButtonElement.removeEventListener('click', setupCloseClickHandler);
-  document.removeEventListener('keydown', setupClosePressEscHandler);
-};
-
-var setupOpenClickHandler = function (evt) {
-  evt.preventDefault();
-  setupElement.classList.remove('hidden');
-
-  setupCloseButtonElement.addEventListener('click', setupCloseClickHandler);
-  document.addEventListener('keydown', setupClosePressEscHandler);
-};
-
-var setupOpenPressEnterHandler = function (evt) {
-  if (evt.key === 'Enter') {
-    evt.preventDefault();
-    setupElement.classList.remove('hidden');
-  }
-
-  setupCloseButtonElement.addEventListener('click', setupCloseClickHandler);
-  document.addEventListener('keydown', setupClosePressEscHandler);
-};
-
 setupOpenButtonElement.addEventListener('click', setupOpenClickHandler);
 setupOpenIconElement.addEventListener('keydown', setupOpenPressEnterHandler);
+
+setupWizardCoatElement.addEventListener('click', function () {
+  var newColor = getRandomArrValue(COAT_COLOR_PATTERN);
+
+  setupWizardCoatElement.style.fill = newColor;
+  setupCoatInputElement.value = newColor;
+});
+
+setupWizardEyesElement.addEventListener('click', function () {
+  var newColor = getRandomArrValue(EYES_COLOR_PATTERN);
+
+  setupWizardEyesElement.style.fill = newColor;
+  setupEyesInputElement.value = newColor;
+});
+
+setupWizardFireballElement.addEventListener('click', function () {
+  var newColor = getRandomArrValue(FIREBALL_COLOR_PATTERN);
+
+  setupWizardFireballElement.style.backgroundColor = newColor;
+  setupFireballInputElement.value = newColor;
+});
